3c37fba252aabe41e1b9f532a5d6dd74afe7239b
root <siraldo.bmx@gmail.com>
2018-11-20 18:18:05 +0000

    Exercicio final, componente controlado

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text: ''
+  }
+
+  updateText = (text) => {
+    this.setState({ text: text.trim() 
+                  })
+  }
+  
   render() {
+    const { text } = this.state
+
+    let whatMirrorText 
+    if (text) {
+      whatMirrorText = `Mirror => ${text}`;
+    } else {
+      whatMirrorText = 'Nothing !'
+    }
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,9 +30,13 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input 
+			type="text" 
+			placeholder="Say Something" 
+			value={text}
+			onChange={(event) => this.updateText(event.target.value)}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{whatMirrorText}</p>
         </div>
       </div>
     );
